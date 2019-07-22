9b224fe662d8c7b1ac9071f244161db048519683
YuYunLong <610671733@qq.com>
2019-07-22 17:15:00 +0800

    doc(): add data table

--- tbg/src/db/createTable.js
+++ tbg/src/db/createTable.js
@@ -4,6 +4,14 @@ const logger = require("../common/logger").child({ "@db/createTable.js": "create
 
 async function createTable() {
     let createTableSql = `
+        comment on table account is '账号表';
+        comment on column account.id is '账户表 id';
+        comment on column account.account_name is '用户 eos 帐号名称';
+        comment on column account.refer_count is '账户创建时间';
+        comment on column account.refer_code is '用户推荐的人数';
+        comment on column account.state is '用户的推荐号';
+        comment on column account.account_type is '账号类型';
+        comment on column account.create_time is '状态';
         CREATE TABLE IF NOT EXISTS account(
             id TEXT PRIMARY KEY UNIQUE NOT NULL DEFAULT '',
             account_name TEXT UNIQUE NOT NULL DEFAULT '',
@@ -13,6 +21,15 @@ async function createTable() {
             account_type TEXT NOT NULL DEFAULT '',
             create_time TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
         );
+        comment on table sub_account is '子帐号表'
+        comment on column sub_account.id is '子帐号表 id';
+        comment on column sub_account.pid is '子帐号在三三公排中对应的父 id';
+        comment on column sub_account.root_node is '三三公排某个根节点帐号';
+        comment on column sub_account.main_account is '子帐号对应的 eos 帐号';
+        comment on column sub_account.sub_account_name is '子帐号名称';
+        comment on column sub_account.level is '在三三公排的层级';
+        comment on column sub_account.position is '子帐号在三三公排对应层级的位置';
+        comment on column sub_account.create_time is '子帐号创建时间';
         CREATE TABLE IF NOT EXISTS sub_account (
             id TEXT PRIMARY KEY UNIQUE NOT NULL DEFAULT '',
             pid TEXT NOT NULL DEFAULT '',
@@ -23,6 +40,14 @@ async function createTable() {
             position INTEGER NOT NULL DEFAULT 0,
             create_time TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
         );
+        comment on table balance is '用户资产表'
+        comment on column balance.id is '用户资产表 id';
+        comment on column balance.account_name is '用户 eos 帐号名称';
+        comment on column balance.withdraw_enable is '可提现资产';
+        comment on column balance.repeat_currency is '复投资产';
+        comment on column balance.lotto_currency is '全球彩彩码';
+        comment on column balance.game_currency is '游戏资产';
+        comment on column balance.create_time is '创建时间';
         CREATE TABLE IF NOT EXISTS balance(
             id TEXT PRIMARY KEY UNIQUE NOT NULL DEFAULT '',
             account_name TEXT UNIQUE NOT NULL DEFAULT '',
@@ -32,12 +57,25 @@ async function createTable() {
             game_currency  NUMERIC (12, 8) NOT NULL DEFAULT 0,
             create_time TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
         );
+        comment on table referrer is '推荐表'
+        comment on column referrer.id is '推荐表 id';
+        comment on column referrer.referrer_name is '推荐人帐号名称';
+        comment on column referrer.account_name is '被推荐人的账户名称';
+        comment on column referrer.create_time is '创建时间';
         CREATE TABLE IF NOT EXISTS referrer(
             id TEXT PRIMARY KEY UNIQUE NOT NULL DEFAULT '',
             referrer_name TEXT,
             account_name TEXT UNIQUE NOT NULL DEFAULT '',
             create_time TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
         );
+        comment on table balance_log is '用户账户余额变动表'
+        comment on column balance_log.id is '变动表 id(自增)';
+        comment on column balance_log.account_name is '用户 eos 帐号名称';
+        comment on column balance_log.change_amount is '变动额度';
+        comment on column balance_log.current_balance is '变动后的余额';
+        comment on column balance_log.op_type is '操作类型';
+        comment on column balance_log.remark is '操作备注';
+        comment on column balance_log.create_time is '创建时间';
         CREATE TABLE IF NOT EXISTS balance_log(
             id serial PRIMARY KEY UNIQUE NOT NULL,
             account_name TEXT NOT NULL DEFAULT '',
@@ -47,11 +85,22 @@ async function createTable() {
             remark TEXT NOT NULL DEFAULT '',
             create_time TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
         );
+        comment on table system_pools is '系统奖池表'
+        comment on column system_pools.id is '系统奖池表的 id';
+        comment on column system_pools.pool_type is '奖池的类型';
+        comment on column system_pools.pool_amount is '奖池的金额';
         CREATE TABLE IF NOT EXISTS system_pools(
             id TEXT PRIMARY KEY UNIQUE NOT NULL DEFAULT '',
             pool_type TEXT UNIQUE NOT NULL DEFAULT '',
             pool_amount NUMERIC (12, 8) NOT NULL DEFAULT 0
         );
+        comment on table system_op_log is '系统总账变动日志表'
+        comment on column system_op_log.id is '奖池变动表的 id';
+        comment on column system_op_log.change_amount is '变动的额度';
+        comment on column system_op_log.current_balance is '变动后的额度';
+        comment on column system_op_log.op_type is '操作类型';
+        comment on column system_op_log.remark is '备注';
+        comment on column system_op_log.create_time is '创建时间';
         CREATE TABLE IF NOT EXISTS system_op_log(
             id serial PRIMARY KEY UNIQUE NOT NULL,
             change_amount NUMERIC (12, 8) NOT NULL DEFAULT 0,
@@ -60,11 +109,68 @@ async function createTable() {
             remark TEXT NOT NULL DEFAULT '',
             create_time TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
         );
+        comment on table account_op is '用户操作记录'
+        comment on column account_op.id is '用户的操作记录表的 id';
+        comment on column account_op.account_name is '用户帐号名称';
+        comment on column account_op.op_type is '用户的操作记录';
+        comment on column account_op.remark is '备注';
+        comment on column account_op.create_time is '创建时间';
         CREATE TABLE IF NOT EXISTS account_op(
             id serial PRIMARY KEY UNIQUE NOT NULL,
             account_name TEXT NOT NULL DEFAULT '',
             op_type TEXT NOT NULL DEFAULT '',
             remark TEXT NOT NULL DEFAULT '',
+            create_time TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
+        );
+        comment on table trade_tbg is '账号表'
+        comment on column trade_tbg.id is '主键 id';
+        comment on column trade_tbg.account_name is '用户帐号名称';
+        comment on column trade_tbg.buy_or_sell is '买或卖';
+        comment on column trade_tbg.amount is '请求交易的数量';
+        comment on column trade_tbg.price is '价格';
+        comment on column trade_tbg.state is '状态';
+        comment on column trade_tbg.create_time is '创建时间';
+        CREATE TABLE IF NOT EXISTS trade_tbg(
+            id serial PRIMARY KEY UNIQUE NOT NULL,
+            account_name TEXT NOT NULL DEFAULT '',
+            buy_or_sell TEXT NOT NULL DEFAULT '',
+            amount TEXT NOT NULL DEFAULT '',
+            price NUMERIC (12, 8) NOT NULL DEFAULT '',
+            state TEXT NOT NULL DEFAULT '',
+            create_time TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
+        );
+        comment on table trade_log is '账号表'
+        comment on column trade_log.id is '主键 id';
+        comment on column trade_log.tr_id is '请求 id';
+        comment on column trade_log.buy_or_sell is '买或卖';
+        comment on column trade_log.amount is '数量';
+        comment on column trade_log.price is '备注说明';
+        comment on column trade_log.volume is '价格';
+        comment on column trade_log.memo is '成交金额';
+        comment on column trade_log.create_time is '创建时间';
+        CREATE TABLE IF NOT EXISTS trade_log(
+            id serial PRIMARY KEY UNIQUE NOT NULL,
+            tr_id TEXT NOT NULL DEFAULT '',
+            buy_or_sell TEXT NOT NULL DEFAULT '',
+            amount TEXT NOT NULL DEFAULT '',
+            price NUMERIC (12, 8) NOT NULL DEFAULT '',
+            volume NUMERIC (12, 8) NOT NULL DEFAULT '',
+            memo TEXT NOT NULL DEFAULT '',
+            create_time TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
+        );
+        comment on table tbg_balance is '用户 TBG 资产表'
+        comment on column tbg_balance.id is '用户资产表';
+        comment on column tbg_balance.account_name is '用户帐号名称';
+        comment on column tbg_balance.release_amount is '释放池资产';
+        comment on column tbg_balance.sell_amount is '可售额度';
+        comment on column tbg_balance.active_amount is '可售余额';
+        comment on column tbg_balance.create_time is '创建时间';
+        CREATE TABLE IF NOT EXISTS tbg_balance(
+            id serial PRIMARY KEY UNIQUE NOT NULL,
+            account_name TEXT NOT NULL DEFAULT '',
+            release_amount NUMERIC (12, 8) NOT NULL DEFAULT 0,
+            sell_amount NUMERIC (12, 8) NOT NULL DEFAULT 0,
+            active_amount  NUMERIC (12, 8) NOT NULL DEFAULT 0,
             create_time TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
         );
     `
