909e2bf6d869eeb097c54b0a8618cbf51c4747ba
YuYunLong <610671733@qq.com>
2019-09-02 17:50:48 +0800

    feat()

--- tbg/src/common/constant/notification.js
+++ tbg/src/common/constant/notification.js
@@ -4,10 +4,15 @@
  */
 const notification = [
     {
+        create_time: "2019-06-23",
+        title: "最新线性释放池调整",
+        description: "最新线性释放池调整"
+    }, 
+    {
         create_time: "2019-06-15",
         title: "新游戏上线公告",
         description: "新游戏上线公告"
-    }, 
+    },
     {
         create_time: "2019-06-12",
         title: "微调公告",
--- tbg/src/controllers/trade/getSellAssetsInfo.js
+++ tbg/src/controllers/trade/getSellAssetsInfo.js
@@ -4,7 +4,7 @@ const logger = require("../../common/logger.js").child({ "@controllers/trade/get
 const { get_status, inspect_req_data } = require("../../common/index.js");
 const { getAccountInfo, getAccountMemberLevel } = require("../../models/account");
 const { redis } = require("../../common");
-const { OPENING_PRICE_KEY, DESTROY, SELL_START_TIME, SELL_END_TIME, SELL_FEE, OPENING_PRICE } = require("../../common/constant/tradeConstant.js");
+const { OPENING_PRICE_KEY, DESTROY, SELL_START_TIME, SELL_END_TIME, SELL_FEE, OPENING_PRICE, BASE_RATE } = require("../../common/constant/tradeConstant.js");
 const { MEMBER_LEVEL_TRX } = require("../../common/constant/assetsConstant.js");
 const { Decimal } = require("decimal.js");
 const { userMember } = require("../../common/userMember.js");
@@ -15,6 +15,7 @@ async function getSellAssetsInfo(req, res, next) {
         const reqData = await inspect_req_data(req);
         logger.debug(`the param is %j: `, reqData);
         const accountName = reqData.account_name;
+        const sellAmount = reqData.amount;
         const accountInfo = await getAccountInfo(accountName);
         if (!accountInfo) {
             return res.send(get_status(1001, "this account does not exists"));
@@ -29,6 +30,17 @@ async function getSellAssetsInfo(req, res, next) {
         let resData = get_status(1);
         // 获取开盘价格
         const openingPrice = await redis.get(OPENING_PRICE_KEY) || OPENING_PRICE;
+        let obj = {}
+        if (!!sellAmount) {
+            const sellFeeAmount = new Decimal(sellAmount).mul(openingPrice).mul(SELL_FEE).div(BASE_RATE).toFixed(4);
+            const income = new Decimal(sellAmount).mul(openingPrice).mul(BASE_RATE - SELL_FEE).div(BASE_RATE).toFixed(4);
+            const destroyAmount = new Decimal(sellAmount).mul(openingPrice).mul(DESTROY).div(BASE_RATE).toFixed(4);
+            obj = {
+                "sell_fee_amount": sellFeeAmount,
+                "destroy_amount": destroyAmount,
+                "income": income
+            }
+        }
         resData.data = {
             "price": new Decimal(openingPrice).toNumber(),
             "sell_fee_rate": SELL_FEE,
@@ -37,7 +49,8 @@ async function getSellAssetsInfo(req, res, next) {
             "one_trx": MEMBER_LEVEL_TRX[levelInfo.ID].ONE_MAX_TRX,
             "day_trx": MEMBER_LEVEL_TRX[levelInfo.ID].DAY_MAX_TRX,
             "trx_day_count": MEMBER_LEVEL_TRX[levelInfo.ID].DAY_TRX_COUNT,
-            "level": levelInfo.NAME
+            "level": levelInfo.NAME,
+            ...obj
         }
         res.send(resData);
     } catch (err) {
