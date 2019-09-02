138e3d4d2ee49295948c3286a77cc5b1f468d0da
D20F <2442563148@qq.com>
2019-09-02 21:19:27 +0800

    修改IP

--- pog_wallet/src/servers/invitation.js
+++ pog_wallet/src/servers/invitation.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // var baseURL = 'http://172.81.224.11/api';
 // var baseURL = 'http://192.168.1.141:9527';
 // var baseURL = 'http://192.168.1.135:9527';
-var baseURL = 'http://172.81.224.11:9090';
+var baseURL = 'http://pog.tbg.isecsp.com';
 
 const xhr = axios.create({
     baseURL,
