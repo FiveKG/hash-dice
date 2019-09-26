13e3e182e92f10a507bcfe8a633b45fbcda209eb
YuYunLong <610671733@qq.com>
2019-09-26 21:04:42 +0800

    f

--- game/global_lotto/src/controllers/global_lotto/gameSessionDetail.js
+++ game/global_lotto/src/controllers/global_lotto/gameSessionDetail.js
@@ -14,7 +14,7 @@ async function gameSessionDetail(req, res, next) {
         let reqData = await inspect_req_data(req);
         logger.debug(`the param is %j: `, reqData);
         const gameInfo = await getGameInfo();;
-        const selectGameSession = `SELECT periods, reward_num, game_state, extra FROM game_session WHERE periods = $1`
+        const selectGameSession = `SELECT periods, reward_num, game_state, extra, reward_time FROM game_session WHERE periods = $1`
         const { rows: [ gameSessionInfo ] } = await pool.query(selectGameSession, [ reqData.periods ]);
         if (!gameSessionInfo) {
             return res.send(get_status(1012, "game not exists"));
@@ -23,7 +23,7 @@ async function gameSessionDetail(req, res, next) {
         let resData = get_status(1);
         if (gameSessionInfo.game_state === GAME_STATE.START || gameSessionInfo.game_state == GAME_STATE.REWARDING) {
             resData.data = {
-                "gs_id": gameSessionInfo.gs_id,
+                "periods": gameSessionInfo.periods,
                 "count_down": df.differenceInSeconds(new Date(), gameSessionInfo.end_time),
                 "reward_time": gameSessionInfo.reward_time,
                 "prize_pool": new Decimal(gameInfo.prize_pool).toFixed(4)
